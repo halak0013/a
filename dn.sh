dad59676a80651a981e7caa15330d98b5cba35b8
Muhammet <75750279+halak0013@users.noreply.github.com>
2022-07-27 12:20:05 +0300

    Update dn.sh

--- dn.sh
+++ dn.sh
@@ -1,1 +1,1 @@
-
+sudo apt-add-repository 'deb http://deb.debian.org/debian bullseye-backports main contrib non-free    deb-src http://deb.debian.org/debian bullseye-backports main contrib non-free'
